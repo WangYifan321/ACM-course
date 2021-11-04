3a959207b1e290e41f4d881043cc7e0fe8ecab57
wangyifan <wangyifan52199@163.com>
2021-11-04 13:51:03 +0800

    change folder name

--- first_homework/1010.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-void main(){
-    double x1,y1,x2,y2,m;
-    while(scanf("%lf %lf %lf %lf",&x1,&y1,&x2,&y2)==4){
-       m = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-       printf("%.2f\n",m);
-    }
-}
